cc75bf22ba77230b8f83ca672294fab22bbd53dc
Nathan <LilFuze9@gmail.com>
2023-11-10 20:43:01 -0500

    thing

--- Program.cs
+++ Program.cs
@@ -1,3 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+
+using System.IO.Compression;
+
+int num1;
+int num2;
+
+Console.Write("Input a number: ");
+num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input a second number: ");
+num2 = Convert.ToInt32(Console.ReadLine());
+
+int result = num1 * num2;
+Console.WriteLine(result);
+
 Console.ReadKey();
+
